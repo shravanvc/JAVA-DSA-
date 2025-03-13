a34a19f8334e69c50a84ae52fdbd026f8b7e6814
shravan <shravankumarc87@gmail.com>
2025-03-13 22:03:18 +0530

    first commit

--- src/Arrays/ArrayListExample.java
+++ src/Arrays/ArrayListExample.java
@@ -1,10 +1,10 @@
-package Arrays;
-
-import java.util.ArrayList;
-
-public class ArrayListExample {
-    public static void main(String[] args) {
-        // Syntax
-        ArrayList<Integer> list = new ArrayList<>();
-    }
-}
+//package Arrays;
+//
+//import java.util.ArrayList;
+//
+//public class ArrayListExample {
+//    public static void main(String[] args) {
+//        // Syntax
+//        ArrayList<Integer> list = new ArrayList<>();
+//    }
+//}
