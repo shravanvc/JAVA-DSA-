805e4d368922d8f8e0bca5945a5965996dd04f34
shravan <shravankumarc87@gmail.com>
2025-03-18 23:02:06 +0530

    first commit

--- src/Arrays/SwapA.java
+++ src/Arrays/SwapA.java
@@ -1,32 +1,32 @@
-//package Arrays;
-//
-//import java.util.Arrays;
-//
-//public class SwapA {
-//    public static void main(String[] args) {
-//
-//        int[] arr = {11, 5, 7, 9, 3, 4, 7, 80};
-////        swap(arr,4,0);
-//
-//        reverse(arr);
-//        System.out.println(Arrays.toString(arr));
-//    }
-//
-//    static void reverse(int[] arr){
-//        int start = 0;
-//        int end = arr.length - 1;
-//
-//        while(start < end){
-//            //swap
-//            swap(arr, start , end);
-//            start++;
-//            end--;
-//        }
-//    }
-//
-//    private static void swap(int[] arr, int index1, int index2) {
-//        int temp = arr[index1];
-//        arr[index1] = arr[index2];
-//        arr[index2] = temp;
-//    }
-//}
+package Arrays;
+
+import java.util.Arrays;
+
+public class SwapA {
+    public static void main(String[] args) {
+
+        int[] arr = {11, 5, 7, 9, 3, 4, 7, 80};
+//        swap(arr,4,0);
+
+        reverse(arr);
+        System.out.println(Arrays.toString(arr));
+    }
+
+    static void reverse(int[] arr){
+        int start = 0;
+        int end = arr.length - 1;
+
+        while(start < end){
+            //swap
+            swap(arr, start , end);
+            start++;
+            end--;
+        }
+    }
+
+    private static void swap(int[] arr, int index1, int index2) {
+        int temp = arr[index1];
+        arr[index1] = arr[index2];
+        arr[index2] = temp;
+    }
+}
