49bc1c5fef8bf4731db1171d22e67555aa055688
shravan <shravankumarc87@gmail.com>
2025-04-12 20:22:48 +0530

    first commit

--- src/LinearSearch/SearchInString.java
+++ src/LinearSearch/SearchInString.java
@@ -1,22 +1,22 @@
-//package LinearSearch;
-//
-//public class SearchInString {
-//    public static void main(String[] args) {
-//        String str = "Shravan";
-//        int target = 'a';
-//        System.out.println(search(str, target));
-//    }
-//
-//    private static boolean search(String str, int target) {
-//        if(str.length() == 0){
-//            return false;
-//        }
-//
-//        for (int i = 0; i < str.length(); i++) {
-//            if(target == str.charAt(i)){
-//                return true;
-//            }
-//        }
-//        return false;
-//    }
-//}
+package LinearSearch;
+
+public class SearchInString {
+    public static void main(String[] args) {
+        String str = "Shravan";
+        int target = 'a';
+        System.out.println(search(str, target));
+    }
+
+    private static boolean search(String str, int target) {
+        if(str.length() == 0){
+            return false;
+        }
+
+        for (int i = 0; i < str.length(); i++) {
+            if(target == str.charAt(i)){
+                return true;
+            }
+        }
+        return false;
+    }
+}
