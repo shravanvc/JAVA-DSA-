4508d921189996cccf6a969d3ec250d4c5df31be
shravan <shravankumarc87@gmail.com>
2025-10-25 22:54:54 +0530

    first commit

--- src/Functions/SumOfFirst.java
+++ src/Functions/SumOfFirst.java
@@ -1,19 +1,19 @@
-//package Functions;
-//
-//import java.util.Scanner;
-//
-//public class SumOfFirst {
-//    public static void main(String[] args) {
-//        Scanner sc = new Scanner(System.in);
-//        System.out.println("Enter the number:");
-//        int n = sc.nextInt();
-//        System.out.println(findSum(n));
-//    }
-//    static int findSum(int n) {
-//        int sum = 0;
-//        for (int x = 1; x <= n; x++)
-//            sum = sum + x;
-//        return sum;
-//    }
-//}
-//
+package Functions;
+
+import java.util.Scanner;
+
+public class SumOfFirst {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter the number:");
+        int n = sc.nextInt();
+        System.out.println(findSum(n));
+    }
+    static int findSum(int n) {
+        int sum = 0;
+        for (int x = 1; x <= n; x++)
+            sum = sum + x;
+        return sum;
+    }
+}
+
