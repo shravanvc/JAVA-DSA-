e9796b94e241e57762f6f65db55460736b158f10
shravan <shravankumarc87@gmail.com>
2025-12-03 20:47:40 +0530

    first commit

--- src/com/shravan/AreaOfCircle.java
+++ src/com/shravan/AreaOfCircle.java
@@ -1,17 +1,18 @@
-package com.shravan;
+//package com.shravan;
+//
+//import java.util.Scanner;
+//
+//public class AreaOfCircle {
+//    public static void main(String[] args) {
+//        Scanner sc = new Scanner(System.in);
+//        System.out.println("Enter the radius of the circle:");
+//        float r = sc.nextFloat();
+//
+//        float PI = 3.14f;
+//        float Area = PI * (r * r);
+//
+//        System.out.println("Area of the circle is :" + Area);
+//
+//    }
 
-import java.util.Scanner;
-
-public class AreaOfCircle {
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        System.out.println("Enter the radius of the circle:");
-        float r = sc.nextFloat();
-
-        float PI = 3.14f;
-        float Area = PI * (r * r);
-
-        System.out.println("Area of the circle is :" + Area);
-
-    }
-}
+//}
