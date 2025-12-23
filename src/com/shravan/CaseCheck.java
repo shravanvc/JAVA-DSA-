d6738d8925901314f8dc24d6ffe1505be1224cd9
shravan <shravankumarc87@gmail.com>
2025-12-23 19:17:47 +0530

    first commit

--- src/com/shravan/CaseCheck.java
+++ src/com/shravan/CaseCheck.java
@@ -1,16 +1,16 @@
-//package com.shravan;
-//
-//import java.util.Scanner;
-//
-//public class CaseCheck {
-//    public static void main(String[] args) {
-//        Scanner sc = new Scanner(System.in);
-//        char ch = sc.next().trim().charAt(0);
-////        String word = "Hello";
-//        if(ch >= 'a' && ch <= 'z'){
-//            System.out.println("Lowercase");
-//        }else {
-//            System.out.println("Uppercase");
-//        }
-//    }
-//}
+package com.shravan;
+
+import java.util.Scanner;
+
+public class CaseCheck {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        char ch = sc.next().trim().charAt(0);
+//        String word = "Hello";
+        if(ch >= 'a' && ch <= 'z'){
+            System.out.println("Lowercase");
+        }else {
+            System.out.println("Uppercase");
+        }
+    }
+}
